b354cb0c8a1688869f9478b33c8bf9ea29798eb0
Wilfredo Duazo Jr <wilfredoduazojr@gmail.com>
2023-04-14 13:56:45 +0800

    Inline moveVertical

--- index.ts
+++ index.ts
@@ -405,7 +405,7 @@ class Up implements Input {
   isUp() { return true; }
   isDown() { return false; }
   handle(): void {
-    moveVertical(-1);
+    map[playery - 1][playerx].moveVertical(-1);
   }
 }
 
@@ -415,7 +415,7 @@ class Down implements Input {
   isUp() { return false; }
   isDown() { return true; }
   handle(): void {
-    moveVertical(1);
+    map[playery + 1][playerx].moveVertical(1);
   }
 }
 
@@ -488,10 +488,6 @@ function moveToTile(newx: number, newy: number) {
   map[newy][newx] = new Player;
   playerx = newx;
   playery = newy;
-}
-
-function moveVertical(dy: number) {
-  map[playery + dy][playerx].moveVertical(dy);
 }
 
 function update() {
